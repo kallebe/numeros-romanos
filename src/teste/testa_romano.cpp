7614a5261a8ee847df11ca0538ed3e85a6fbc2ac
Kallebe <kallebe87@gmail.com>
2019-04-13 17:20:44 -0300

    Inclusão de novas condições de teste

--- src/teste/testa_romano.cpp
+++ src/teste/testa_romano.cpp
@@ -4,10 +4,21 @@
 #include "gtest/gtest.h"
 
 TEST(Conversao, TesteNumerosSimples) {
-    char numero_romano[] = "I";
-    int numero_arabico = 1;
+    char numero_r_um[]          = "I";
+    char numero_r_cinco[]       = "V";
+    char numero_r_dez[]         = "X";
+    char numero_r_cinquenta[]   = "L";
+    char numero_r_cem[]         = "C";
+    char numero_r_quinhentos[]  = "D";
+    char numero_r_mil[]         = "M";
 
-    EXPECT_EQ(converteNumeroRomano(numero_romano), numero_arabico);
+    EXPECT_EQ(converteNumeroRomano(numero_r_um), 1);
+    EXPECT_EQ(converteNumeroRomano(numero_r_cinco), 5);
+    EXPECT_EQ(converteNumeroRomano(numero_r_dez), 10);
+    EXPECT_EQ(converteNumeroRomano(numero_r_cinquenta), 50);
+    EXPECT_EQ(converteNumeroRomano(numero_r_cem), 100);
+    EXPECT_EQ(converteNumeroRomano(numero_r_quinhentos), 500);
+    EXPECT_EQ(converteNumeroRomano(numero_r_mil), 1000);
 }
 
 int main(int argc, char **argv) {
