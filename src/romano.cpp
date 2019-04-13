4f1b89b03c6fd47106bc61d9b45b12a9a237a933
Kallebe <kallebe87@gmail.com>
2019-04-13 16:46:21 -0300

    Implementação do primeiro teste

--- src/romano.cpp
+++ src/romano.cpp
@@ -1,5 +1,19 @@
 // Copyright (c) 2019 Kallebe de Sousa Silva. All rights reserved.
 
 int converteNumeroRomano(char numero_romano[]) {
+    int num = 0, i = 0;
+
+    while(numero_romano[i] != '\n') {
+        switch (numero_romano[i])
+        {
+            case 'I':
+                return 1;
+                break;
+            default:
+                return -1;
+                break;
+        }
+    }
+
     return 0;
 }
