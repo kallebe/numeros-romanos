06096303d22b19cbc599cb0bc2246536230049da
Kallebe <kallebe87@gmail.com>
2019-04-13 18:01:24 -0300

    Implementação das condições do teste 1

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,18 @@
 #include "romano.hpp"
 
 int main() {
+    char numero_romano[31];
+    int num = -1;
+
+    printf("Digite o numero romano: ");
+    scanf("%30s", numero_romano);
+
+    num = converteNumeroRomano(numero_romano);
+    if (num != -1) {
+        printf("O numero eh: %d\n", num);
+    } else {
+        printf("O numero digitado eh invalido!\n");
+    }
+
     return 0;
 }
--- src/romano.cpp
+++ src/romano.cpp
@@ -3,11 +3,28 @@
 int converteNumeroRomano(char numero_romano[]) {
     int num = 0, i = 0;
 
-    while(numero_romano[i] != '\n') {
-        switch (numero_romano[i])
-        {
+    while (numero_romano[i] != '\n') {
+        switch (numero_romano[i]) {
             case 'I':
                 return 1;
+                break;
+            case 'V':
+                return 5;
+                break;
+            case 'X':
+                return 10;
+                break;
+            case 'L':
+                return 50;
+                break;
+            case 'C':
+                return 100;
+                break;
+            case 'D':
+                return 500;
+                break;
+            case 'M':
+                return 1000;
                 break;
             default:
                 return -1;
