57b77193d6d92a8c149b07f87718b63b4d2671c4
Kallebe <kallebe87@gmail.com>
2019-04-14 16:47:31 -0300

    Refatoração de código

--- src/romano.cpp
+++ src/romano.cpp
@@ -9,23 +9,29 @@ int converteNumeroRomano(char num_romano[]) {
         num_atual = converteAlgarismo(num_romano[i]);
         prox_num = converteAlgarismo(num_romano[i+1]);
 
-        if (num_atual == prox_num)
-            cont_rep++;
-        else
-            cont_rep = 1;
-
-        if ((num_atual == 5 || num_atual == 50 || num_atual == 500) && cont_rep == 2)
-            return -1;
-
-        if (cont_rep > 3)
-            return -1;
-
         if (prox_num > num_atual) {
-            if (prox_num - num_atual == 5 || prox_num - num_atual == 50 || prox_num - num_atual == 500)
+            if (prox_num == 2*num_atual)    // Casos como: VX, LC, DM
                 return -1;
             num -= num_atual;
-        } else
+            cont_rep = 1;
+        } else {
+            if (prox_num == num_atual) {
+                switch (num_atual) {        // Casos como: VV, LL, DD
+                    case 5:
+                    case 50:
+                    case 500:
+                        return -1;
+                    default:
+                        cont_rep++;
+                        break;
+                }
+            } else {
+                cont_rep = 1;
+            }
             num += num_atual;
+        }
+        if (cont_rep > 3)
+            return -1;
         i++;
     }
 
@@ -36,27 +42,19 @@ int converteAlgarismo(char num_romano) {
     switch (num_romano) {
         case 'I':
             return 1;
-            break;
         case 'V':
             return 5;
-            break;
         case 'X':
             return 10;
-            break;
         case 'L':
             return 50;
-            break;
         case 'C':
             return 100;
-            break;
         case 'D':
             return 500;
-            break;
         case 'M':
             return 1000;
-            break;
         default:    // Caso seja '\0' retornará 0
             return 0;
-            break;
     }
 }
