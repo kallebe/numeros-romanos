9add84014315f940da2eb2ab48941583ed0bfa74
Kallebe <kallebe87@gmail.com>
2019-04-14 17:53:39 -0300

    Uso do padrão da lingua portuguesa

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 // Copyright (c) 2019 Kallebe de Sousa Silva. All rights reserved.
 
 #include <stdio.h>
+#include <locale.h>
 #include "romano.hpp"
 
 int main() {
+    setlocale(LC_ALL, "Portuguese");
     char numero_romano[31];
     int num = -1;
 
@@ -12,9 +14,9 @@ int main() {
 
     num = converteNumeroRomano(numero_romano);
     if (num != -1) {
-        printf("O numero eh: %d\n", num);
+        printf("O numero é: %d\n", num);
     } else {
-        printf("O numero digitado eh invalido!\n");
+        printf("O numero digitado é invalido!\n");
     }
 
     return 0;
