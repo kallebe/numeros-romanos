fd0ece6a1d6e1521ad8c1a24aef92d0120cb9af4
Kallebe <kallebe87@gmail.com>
2019-04-14 12:58:56 -0300

    Adição de novos casos de teste [PASSED]

--- src/teste/testa_romano.cpp
+++ src/teste/testa_romano.cpp
@@ -4,39 +4,45 @@
 #include "gtest/gtest.h"
 
 TEST(Conversao, TesteNumerosSimples) {
-    char numero_r_um[]          = "I";
-    char numero_r_cinco[]       = "V";
-    char numero_r_dez[]         = "X";
-    char numero_r_cinquenta[]   = "L";
-    char numero_r_cem[]         = "C";
-    char numero_r_quinhentos[]  = "D";
-    char numero_r_mil[]         = "M";
+    char num_r_um[]          = "I";
+    char num_r_cinco[]       = "V";
+    char num_r_dez[]         = "X";
+    char num_r_cinquenta[]   = "L";
+    char num_r_cem[]         = "C";
+    char num_r_quinhentos[]  = "D";
+    char num_r_mil[]         = "M";
 
-    EXPECT_EQ(converteNumeroRomano(numero_r_um), 1);
-    EXPECT_EQ(converteNumeroRomano(numero_r_cinco), 5);
-    EXPECT_EQ(converteNumeroRomano(numero_r_dez), 10);
-    EXPECT_EQ(converteNumeroRomano(numero_r_cinquenta), 50);
-    EXPECT_EQ(converteNumeroRomano(numero_r_cem), 100);
-    EXPECT_EQ(converteNumeroRomano(numero_r_quinhentos), 500);
-    EXPECT_EQ(converteNumeroRomano(numero_r_mil), 1000);
+    EXPECT_EQ(converteNumeroRomano(num_r_um), 1);
+    EXPECT_EQ(converteNumeroRomano(num_r_cinco), 5);
+    EXPECT_EQ(converteNumeroRomano(num_r_dez), 10);
+    EXPECT_EQ(converteNumeroRomano(num_r_cinquenta), 50);
+    EXPECT_EQ(converteNumeroRomano(num_r_cem), 100);
+    EXPECT_EQ(converteNumeroRomano(num_r_quinhentos), 500);
+    EXPECT_EQ(converteNumeroRomano(num_r_mil), 1000);
 }
 
 TEST(Conversao, TesteNumerosCompostos) {
-    char numero_r_dois[]                = "II";
-    char numero_r_quatro[]              = "IV";
-    char numero_r_seis[]                = "VI";
-    char numero_r_sessenta_tres[]       = "LXIII";
-    char numero_r_cento_quarenta_nove[] = "CXLIX";
-    char numero_r_quinhentos_vinte[]    = "DXX";
-    char numero_r_dois_mil_trezentos[]  = "MMCCC";
+    char num_r_dois[]                       = "II";
+    char num_r_quatro[]                     = "IV";
+    char num_r_seis[]                       = "VI";
+    char num_r_trinta[]                     = "XXX";
+    char num_r_sessenta_tres[]              = "LXIII";
+    char num_r_cento_quarenta_nove[]        = "CXLIX";
+    char num_r_trezentos_oitenta_oito[]     = "CCCLXXXVIII";
+    char num_r_quinhentos_vinte[]           = "DXX";
+    char num_r_dois_mil_trezentos[]         = "MMCCC";
+    char num_r_dois_mil_novc_setenta_nove[] = "MMCMLXXIX";
 
-    EXPECT_EQ(converteNumeroRomano(numero_r_dois), 2);
-    EXPECT_EQ(converteNumeroRomano(numero_r_quatro), 4);
-    EXPECT_EQ(converteNumeroRomano(numero_r_seis), 6);
-    EXPECT_EQ(converteNumeroRomano(numero_r_sessenta_tres), 63);
-    EXPECT_EQ(converteNumeroRomano(numero_r_cento_quarenta_nove), 149);
-    EXPECT_EQ(converteNumeroRomano(numero_r_quinhentos_vinte), 520);
-    EXPECT_EQ(converteNumeroRomano(numero_r_dois_mil_trezentos), 2300);
+    EXPECT_EQ(converteNumeroRomano(num_r_dois), 2);
+    EXPECT_EQ(converteNumeroRomano(num_r_quatro), 4);
+    EXPECT_EQ(converteNumeroRomano(num_r_seis), 6);
+    EXPECT_EQ(converteNumeroRomano(num_r_trinta), 30);
+    EXPECT_EQ(converteNumeroRomano(num_r_sessenta_tres), 63);
+    EXPECT_EQ(converteNumeroRomano(num_r_cento_quarenta_nove), 149);
+    EXPECT_EQ(converteNumeroRomano(num_r_trezentos_oitenta_oito), 388);
+    EXPECT_EQ(converteNumeroRomano(num_r_quinhentos_vinte), 520);
+    EXPECT_EQ(converteNumeroRomano(num_r_dois_mil_trezentos), 2300);
+    EXPECT_EQ(converteNumeroRomano(num_r_dois_mil_novc_setenta_nove), 2979);
 }
 
 int main(int argc, char **argv) {
